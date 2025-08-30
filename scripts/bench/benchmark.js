f5be4ec30b40d0066a5963ddb731c99aaa0c07fc
cavassani <lucas.cavassani.q@gmail.com>
2025-08-30 03:56:50 -0300

    implementação de benchmark no seu respectivo diretório

---
+++ scripts/bench/benchmark.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const Lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+
+const stats = require('stats-analysis');
+const config = require('lighthouse/lighthouse-core/config/perf-config');
+const spawn = require('child_process').spawn;
+const os = require('os');
+
+const timesToRun = 10;
+
+function wait(val) {
+    return new Promise(resolve => setTimeout(resolve, val));
+}
+
+async function runScenario(benchmark, chrome) {
+    const port = chrome.port;
+
+    const results = await Lighthouse(
+        `http://localhost:8080/${benchmark}/`, {
+            output: 'json',
+            port
+        },
+
+        config
+    );
+
+    const perfMarkings = results.lhr.audits['user-timings'].details.items;
+
+    const entries = perfMarkings
+        .filter(({timingType}) => timingType !== 'Mark')
+        .map(({duration, name}) => ({
+            entry: name,
+            time: duration
+        }));
+
+    entries.push({
+        entry: 'first meaningful paint',
+        time: results.lhr.audits['first-meaningful-paint'].rawValue
+    });
+
+    return entries;
+}
+
+function bootstrap(data) {
+    const len = data.length;
+    const arr = Array(len);
+
+    for (let j = 0; j < len; j++) {
+        arr[j] = data[(Math.random() * len) | 0];
+    }
+
+    return arr;
+}
+
+function calculateStandardErrorOfMean(data) {
+    const means = [];
+
+    for (let i = 0; i < 10000; i++) {
+        means.push(stats.mean(bootstrap(data)));
+    }
+
+    return stats.stdev(means);
+}
+
+async function launchChrome(headless) {
+    return await chromeLauncher.launch({
+        chromeFlags: [headless ? '--headless' : '']
+    });
+}
+
+module.exports = runBenchmark;
